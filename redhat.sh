9828a69702ebf423bde6746092aace28e30feb01
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-08-10 19:00:01 -0700

    Create redhat.sh

---
+++ redhat.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cd / 
+rm -rf DataSurgeon
+
+: <<'END_COMMENT'
+This script performs the following actions on a Red Hat UBI8 Docker instance:
+  1. Updates the Docker instance's system packages.
+  2. Installs necessary dependencies such as curl, wget, git, and other build tools.
+  3. Installs the Rust programming language.
+  4. Installs DataSurgeon from its GitHub repository.
+  5. Renames the DataSurgeon executable to include its version, platform, and architecture details.
+
+Usage:
+  ./redhat.sh
+
+To copy the executable from this Docker container to your local machine:
+  1. On your host machine, find the container's ID with:
+     docker ps
+
+  2. Note the ID of your running container.
+
+  3. Use docker cp to copy the executable:
+     docker cp CONTAINER_ID:/DataSurgeon/target/release/ds-v${DS_VERSION}-redhat-${ARCHITECTURE} /path/on/your/host/machine
+END_COMMENT
+
+DS_VERSION="1.2.4"
+
+# Update the docker instance
+yum update -y
+
+# Install the dependencies 
+yum install -y curl wget git pkgconfig openssl-devel gcc make
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
+# Clone and setup DataSurgeon
+git clone https://github.com/Drew-Alleman/DataSurgeon
+cd DataSurgeon 
+
+# Add the required dependencies to Cargo.toml
+echo "[dependencies.openssl-sys]" >> Cargo.toml
+echo "version = \"0.9\"" >> Cargo.toml
+echo "features = [\"vendored\"]" >> Cargo.toml
+
+# Build the project
+cargo build --release
+
+# Identify the architecture
+ARCHITECTURE=$(uname -m)
+
+# Rename the executable
+mv target/release/ds target/release/ds-v${DS_VERSION}-redhat-${ARCHITECTURE}
